8e69d3d590f995a6b79101b7943192d999e53d28
nina75 <kozasbrada28>
2016-04-29 01:49:45 +0300

    Problem 1 - Allocate array

---
+++ Homework_Arrays/AllocateArray/AllocateArr.cs
@@ -0,0 +1,15 @@
+﻿//Write a program that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+//Print the obtained array on the console.
+
+class AllocateArr
+{
+    static void Main()
+    {
+        var arr = new int[20];
+        for (int i = 0; i < 20; i++)
+        {
+            arr[i] = i * 5;
+        }
+        System.Console.WriteLine(string.Join(", ", arr));
+    }
+}
