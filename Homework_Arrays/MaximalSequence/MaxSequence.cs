3526479235f5db050c7df538f426c30d7a4c03b3
nina75 <kozasbrada28>
2016-04-29 02:56:10 +0300

    Problem 4 - Maximal sequence

---
+++ Homework_Arrays/MaximalSequence/MaxSequence.cs
@@ -0,0 +1,34 @@
+﻿//Write a program that finds the maximal sequence of equal elements in an array.
+
+using System;
+
+class MaxSequence
+{
+    static void Main()
+    {
+        int[] arr = { 2, 1, 1, 2, 3, 3, 2, 2, 2, 1 };
+        int maxCount = 1, currentCount = 1, element = arr[0];
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] == arr[i + 1])
+            {
+                currentCount++;
+            }
+            else
+            {
+                currentCount = 1;
+            }
+            if (currentCount > maxCount)
+            {
+                maxCount = currentCount;
+                element = arr[i];
+            }
+        }
+        for (int i = 0; i < maxCount - 1; i++)
+        {
+            Console.Write(element + ", ");
+        }
+        Console.WriteLine(element);
+    }
+
+}
